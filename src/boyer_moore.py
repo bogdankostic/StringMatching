0bb9df3e79304749774a8f2af82bb887c88b4014
Bogdan Kostić <bogdankostic@web.de>
2021-03-08 13:45:13 +0100

    Refactor good suffix function

--- src/boyer_moore.py
+++ src/boyer_moore.py
@@ -9,30 +9,37 @@ class BoyerMooreMatcher:
         if not self.case_sensitive:
             pattern = pattern.lower()
             text = text.lower()
-            
-        last_occurrence_dict = self._compute_last_occurrence_dict(pattern)
-        good_suffix_dict = self._compute_good_suffix_dict(pattern)
 
-        correct_shifts = []
-        text_len = len(text)
         pattern_len = len(pattern)
+        text_len = len(text)
 
+        last_occurence_dict = self._compute_last_occurrence_dict(pattern)
+        good_suffix_dict = self._compute_good_suffix_dict(pattern)
         current_shift = 0
-        while current_shift < text_len - pattern_len + 1:
-            current_pattern_idx = pattern_len - 1
-            while current_pattern_idx and \
-                  pattern[current_pattern_idx] == text[current_shift + current_pattern_idx]:
-                current_pattern_idx -= 1
+        good_shifts = []
 
-            if current_pattern_idx == 0:
-                correct_shifts.append(current_shift)
-                s += ...
+        while current_shift < text_len - pattern_len:
+            current_char_idx = pattern_len - 1
+            while current_char_idx >= 0 \
+                  and pattern[current_char_idx] == text[current_shift + current_char_idx]:
+                current_char_idx -= 1
+
+            if current_char_idx < 0:
+                good_shifts.append(current_shift)
+                current_shift += good_suffix_dict[-1]
             else:
-                s += ...
+                bad_char = text[current_shift + current_char_idx]
+                current_shift += max(
+                    good_suffix_dict[current_char_idx],
+                    current_char_idx - last_occurence_dict[bad_char]
+                )
+
+        return good_shifts
+
 
     @staticmethod
     def _compute_last_occurrence_dict(pattern: str):
-        last_occurrence_dict = defaultdict(0)
+        last_occurrence_dict = defaultdict(lambda: -1)
         for idx, char in enumerate(pattern):
             last_occurrence_dict[char] = idx
             
@@ -41,38 +48,38 @@ class BoyerMooreMatcher:
     @staticmethod
     def _compute_prefix_dict(pattern: str):
         pattern_len = len(pattern)
-        prefix_dict = {}
-        prefix_dict[0] = 0
-        k = 0
+        prefix_dict = {0: 0}
+        good_prefix_len = 0
 
-        for q in range(1, pattern_len):
-            while k > 0 and pattern[k] != pattern[q]:
-                k = prefix_dict[k]
+        for current_idx in range(1, pattern_len):
+            while good_prefix_len > 0 \
+                  and pattern[good_prefix_len] != pattern[current_idx]:
+                good_prefix_len = prefix_dict[good_prefix_len]
 
-            if pattern[k] == pattern[q]:
-                k += 1
+            if pattern[good_prefix_len] == pattern[current_idx]:
+                good_prefix_len += 1
 
-            prefix_dict[q] = k
+            prefix_dict[current_idx] = good_prefix_len
 
         return prefix_dict
 
     def _compute_good_suffix_dict(self, pattern: str):
-        good_suffix_dict = {}
-        
         pattern_len = len(pattern)
-        
         pattern_prefix_dict = self._compute_prefix_dict(pattern)
+
         reversed_pattern = pattern[::-1]
         reversed_pattern_prefix_dict = self._compute_prefix_dict(reversed_pattern)
-        
-        for j in range(-1, pattern_len):
-            good_suffix_dict[j] = pattern_len - pattern_prefix_dict[pattern_len]
-            
-        for l in range(pattern_len):
-            j = pattern_len - reversed_pattern_prefix_dict[l]
-            if good_suffix_dict[j] > l - reversed_pattern_prefix_dict[l]:
-                good_suffix_dict[j] = l - reversed_pattern_prefix_dict[l]
-                
+
+        good_suffix_dict = {}
+        for idx in range(-1, pattern_len):
+            good_suffix_dict[idx] = pattern_len - pattern_prefix_dict[pattern_len - 1]
+
+        for idx in range(pattern_len):
+            j = pattern_len - 1 - reversed_pattern_prefix_dict[idx]
+            new_value = idx - reversed_pattern_prefix_dict[idx]
+            if good_suffix_dict[j] > new_value:
+                good_suffix_dict[j] = new_value
+
         return good_suffix_dict
             
         
